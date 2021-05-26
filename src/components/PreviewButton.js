3c89f7bff8e9ae3de701ca789b34b1d48cfb3d77
Scott Alton <scottaltonmusic@gmail.com>
2021-05-26 13:58:05 -0400

    Added toggle preview/edit mode button.

--- src/components/PreviewButton.js
+++ src/components/PreviewButton.js
@@ -15,12 +15,24 @@ class PreviewButton extends Component {
 
     let buttons = document.querySelectorAll('button');
     let previewButton = document.querySelector('.btn-preview');
+    let forms = document.querySelectorAll('form');
 
     buttons.forEach((button) => {
       if (button.style.display !== 'none') {
         button.style.display = 'none';
       } else {
         button.style.display = 'block';
+        if (!this.state.preview) {
+          document.querySelector('.add-form-buttons').style.display = 'flex';
+        }
+      }
+    });
+
+    forms.forEach((form) => {
+      if (form.style.display !== 'none') {
+        form.style.display = 'none';
+      } else {
+        form.style.display = 'grid';
       }
     });
 
@@ -29,7 +41,7 @@ class PreviewButton extends Component {
 
   render() {
     return (
-      <button onClick={this.togglePreview} className="btn-preview">
+      <button onClick={this.togglePreview} className="btn btn-preview">
         {this.state.preview ? 'Edit' : 'Preview'}
       </button>
     );
