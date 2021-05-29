c95c3a147ee36d1a89614bcc7fb7d408e00fdbaf
Scott Alton <scottaltonmusic@gmail.com>
2021-05-29 12:44:43 -0400

    Refactored PreviewButton to use Hooks.

--- src/components/PreviewButton.js
+++ src/components/PreviewButton.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class PreviewButton extends Component {
-  constructor(props) {
-    super(props);
-
-    // Hold state of user mode and potentially removed placeholder contents
-    this.state = {
-      preview: false,
-      placeholders: [],
-    };
-  }
+const PreviewButton = () => {
+  // Hold state of user mode and potentially removed placeholder contents
+  const [preview, setPreview] = useState(false);
+  const [placeholders, setPlaceholders] = useState([]);
 
   // Switch between preview and edit mode
-  togglePreview = (e) => {
+  const togglePreview = (e) => {
     e.preventDefault();
-    this.setState({ preview: !this.state.preview });
+    setPreview(!preview);
 
     // Grab reference to all controls to be removed in preview mode and added back in for edit mode
     let buttons = document.querySelectorAll('button');
     let previewButton = document.querySelector('.btn-preview');
     let forms = document.querySelectorAll('form');
     let inputs = document.querySelectorAll('.resume-header input');
-    let placeholders = [];
 
     // Hide or show buttons depending on user mode
     buttons.forEach((button) => {
@@ -29,7 +22,7 @@ class PreviewButton extends Component {
         button.style.display = 'none';
       } else {
         button.style.display = 'block';
-        if (!this.state.preview) {
+        if (!preview) {
           document.querySelector('.add-form-buttons').style.display = 'flex';
         }
       }
@@ -47,19 +40,17 @@ class PreviewButton extends Component {
     previewButton.style.display = 'block';
 
     // Call function to restore placeholders when in edit mode to guide user
-    this.restorePlaceholderText(placeholders, inputs);
+    restorePlaceholderText(placeholders, inputs);
   };
 
   // Regenerate or gather placeholders depending on user mode
-  restorePlaceholderText = (placeholders, inputs) => {
-    if (!this.state.preview) {
+  const restorePlaceholderText = (placeholders, inputs) => {
+    if (!preview) {
       // Add all placeholder text to state for reuse when toggling back to edit mode
       inputs.forEach((input) => {
         placeholders.push(input.getAttribute('placeholder'));
 
-        this.setState({
-          placeholders,
-        });
+        setPlaceholders(placeholders);
 
         // Once stored, remove all placeholder values
         input.setAttribute('placeholder', '');
@@ -67,18 +58,16 @@ class PreviewButton extends Component {
     } else {
       // If going back to edit mode, reassign placeholder text to inputs in resume header section
       inputs.forEach((input, index) => {
-        input.setAttribute('placeholder', this.state.placeholders[index]);
+        input.setAttribute('placeholder', placeholders[index]);
       });
     }
   };
 
-  render() {
-    return (
-      <button onClick={this.togglePreview} className="btn btn-preview">
-        {this.state.preview ? 'Edit' : 'Preview'}
-      </button>
-    );
-  }
-}
+  return (
+    <button onClick={togglePreview} className="btn btn-preview">
+      {preview ? 'Edit' : 'Preview'}
+    </button>
+  );
+};
 
 export default PreviewButton;
