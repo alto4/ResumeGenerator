c4c0d9df3b3059328b5dd7d6c973cb828ec39b8a
Scott Alton <scottaltonmusic@gmail.com>
2021-05-29 13:45:21 -0400

    Refactored education section and resume component to use hooks.

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,84 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class EducationSection extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Store state of form display and form fields
-    this.state = {
-      showForm: false,
-      showAddForm: false,
-      showEditForm: false,
-      entryEditing: null,
-      credential: '',
-      school: '',
-      date: '',
-      program: '',
-      description: [],
-    };
-  }
+const EducationSection = (props) => {
+  // Store state of form display and form fields
+  const [showForm, setShowForm] = useState(false);
+  const [displayAddForm, setDisplayAddForm] = useState(false);
+  const [displayEditForm, setDisplayEditForm] = useState(false);
+  const [entryEditing, setEntryEditing] = useState(null);
+  const [credential, setCredential] = useState('');
+  const [school, setSchool] = useState('');
+  const [date, setDate] = useState('');
+  const [program, setProgram] = useState('');
+  const [description, setDescription] = useState([]);
 
   // Show/hide education entry form
-  toggleFormDisplay = () => {
-    this.setState({ showForm: !this.state.showForm });
+  const toggleFormDisplay = () => {
+    setShowForm(!showForm);
+  };
+
+  // Clear potentially populated inputs to default state
+  const clearFormFields = () => {
+    setCredential('');
+    setProgram('');
+    setSchool('');
+    setDate('');
+    setDescription('');
   };
 
   // Display add form after clearing potentially populated fields
-  showAddForm = (e) => {
-    this.clearFormFields();
+  const showAddForm = (e) => {
+    clearFormFields();
     e.preventDefault();
 
     // Ensure that clicking add button always results in an open form
-    if (this.state.showForm === false) {
-      this.toggleFormDisplay();
+    if (showForm === false) {
+      toggleFormDisplay();
     }
 
     // Only ever allow a single input form to be displayed
-    this.setState({ showAddForm: true, showEditForm: false });
+    setDisplayAddForm(true);
+    setDisplayEditForm(false);
   };
 
   // Display edit form
-  showEditForm = (e) => {
+  const showEditForm = (e) => {
     e.preventDefault();
 
     // Ensure that clicking edit button always results in an open form
-    if (this.state.showForm === false) {
-      this.toggleFormDisplay();
+    if (showForm === false) {
+      toggleFormDisplay();
     }
 
     // Update state to current form status and capture id of entry being targetted
-    this.setState({
-      showEditForm: true,
-      showAddForm: false,
-      entryEditing: e.currentTarget.getAttribute('data-id'),
-    });
+    setDisplayEditForm(true);
+    setDisplayAddForm(false);
+    setEntryEditing(e.currentTarget.getAttribute('data-id'));
 
     // Populate edit form fields with data for corresponding entry
     let { credential, program, school, date, description } =
-      this.props.entries[e.currentTarget.getAttribute('data-id')];
+      props.entries[e.currentTarget.getAttribute('data-id')];
 
-    this.setState({ credential, program, school, date, description });
+    setCredential(credential);
+    setProgram(program);
+    setSchool(school);
+    setDate(date);
+    setDescription(description);
   };
 
   // Cancel add or edit form entry in progress
-  cancelEntry = (e) => {
+  const cancelEntry = (e) => {
     e.preventDefault();
 
-    this.toggleFormDisplay();
+    toggleFormDisplay();
   };
 
   // Submit new education entry based on current state of form fields
-  addNewEntry = (e) => {
+  const addNewEntry = (e) => {
     e.preventDefault();
 
-    let credential = this.state.credential;
-    let program = this.state.program;
-    let school = this.state.school;
-    let date = this.state.date;
-    let description = this.state.description;
-
     // Pass new entry up to Resume component
-    this.props.addEducation(
+    props.addEducation(
       {
         credential,
         program,
@@ -90,180 +90,167 @@ class EducationSection extends React.Component {
     );
 
     // Close form and clear all fields
-    this.setState({ showAddForm: !this.state.showAddForm });
-    this.toggleFormDisplay();
-    this.clearFormFields();
+    setDisplayAddForm(!showAddForm);
+    toggleFormDisplay();
+    clearFormFields();
   };
 
   // Overwrite existing education entry and pass up to Resume component
-  editEntry = (e) => {
+  const editEntry = (e) => {
     e.preventDefault();
 
-    this.props.editEducation(this.state.entryEditing, 'education', {
-      credential: this.state.credential,
-      date: this.state.date,
-      school: this.state.school,
-      program: this.state.program,
-      description: this.state.description.split(','),
+    props.editEducation(entryEditing, 'education', {
+      credential,
+      date,
+      school,
+      program,
+      description: description.split(','),
     });
 
-    this.toggleFormDisplay();
-    this.clearFormFields();
+    toggleFormDisplay();
+    clearFormFields();
   };
 
   // Remove targetted education entry from Resume component state array
-  removeEntry = (e) => {
+  const removeEntry = (e) => {
     e.preventDefault();
 
-    this.props.removeEducation(e.target.getAttribute('data-id'), 'education');
+    props.removeEducation(e.target.getAttribute('data-id'), 'education');
   };
 
-  // Clear potentially populated inputs to default state
-  clearFormFields = () => {
-    this.setState({
-      credential: '',
-      program: '',
-      school: '',
-      date: '',
-      description: '',
-    });
-  };
+  return (
+    <div className="education-section">
+      <h2>Education</h2>
+      <button
+        className="btn btn-add"
+        onClick={(toggleFormDisplay, showAddForm)}
+      >
+        Add New Education
+      </button>
 
-  // Handle change made to value of inputs
-  onChange = (e) => {
-    let target = e.target.name;
-    let value = e.target.value;
+      {showForm && (
+        <form>
+          {displayAddForm && <h4>Add New Education Entry</h4>}
+          {displayEditForm && <h4>Edit Education Entry</h4>}
 
-    this.setState((state, props) => ({
-      [target]: value,
-    }));
-  };
+          <label htmlFor="credential">Credential:</label>
+          <input
+            type="text"
+            name="credential"
+            onChange={(e) => {
+              setCredential(e.target.value);
+            }}
+            value={credential}
+          />
 
-  render() {
-    return (
-      <div className="education-section">
-        <h2>Education</h2>
-        <button
-          className="btn btn-add"
-          onClick={(this.toggleFormDisplay, this.showAddForm)}
-        >
-          Add New Education
-        </button>
+          <label htmlFor="program">Program:</label>
+          <input
+            type="text"
+            name="program"
+            onChange={(e) => {
+              setProgram(e.target.value);
+            }}
+            value={program}
+          />
 
-        {this.state.showForm && (
-          <form>
-            {this.state.showAddForm && <h4>Add New Education Entry</h4>}
-            {this.state.showEditForm && <h4>Edit Education Entry</h4>}
+          <label htmlFor="school">School:</label>
+          <input
+            type="text"
+            name="school"
+            onChange={(e) => {
+              setSchool(e.target.value);
+            }}
+            value={school}
+          />
 
-            <label htmlFor="credential">Credential:</label>
-            <input
-              type="text"
-              name="credential"
-              onChange={this.onChange}
-              value={this.state.credential}
-            />
+          <label htmlFor="date">Date:</label>
+          <input
+            type="text"
+            name="date"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+            value={date}
+          />
 
-            <label htmlFor="program">Program:</label>
-            <input
-              type="text"
-              name="program"
-              onChange={this.onChange}
-              value={this.state.program}
-            />
+          <label htmlFor="description">Description:</label>
+          <input
+            type="text"
+            name="description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            description={description}
+          />
 
-            <label htmlFor="school">School:</label>
-            <input
-              type="text"
-              name="school"
-              onChange={this.onChange}
-              value={this.state.school}
-            />
+          {displayAddForm && (
+            <div className="add-form-buttons">
+              <button className="btn btn-add" onClick={addNewEntry}>
+                Add
+              </button>
+              <button className="btn btn-cancel" onClick={cancelEntry}>
+                Cancel
+              </button>
+            </div>
+          )}
 
-            <label htmlFor="date">Date:</label>
-            <input
-              type="text"
-              name="date"
-              onChange={this.onChange}
-              value={this.state.date}
-            />
+          {displayEditForm && (
+            <div className="edit-form-buttons">
+              <button className="btn btn-edit" onClick={editEntry}>
+                Edit
+              </button>
+              <button className="btn btn-cancel" onClick={cancelEntry}>
+                Cancel
+              </button>
+            </div>
+          )}
+        </form>
+      )}
 
-            <label htmlFor="description">Description:</label>
-            <input
-              type="text"
-              name="description"
-              onChange={this.onChange}
-              description={this.state.description}
-            />
+      <div className="education-details-container">
+        <article>
+          {props.entries.map((entry, index) => {
+            return (
+              <div className="education-entry" key={index}>
+                <div className="education-entry-header">
+                  <h3>
+                    {entry.credential} - {entry.program}
+                  </h3>
 
-            {this.state.showAddForm && (
-              <div className="add-form-buttons">
-                <button className="btn btn-add" onClick={this.addNewEntry}>
-                  Add
-                </button>
-                <button className="btn btn-cancel" onClick={this.cancelEntry}>
-                  Cancel
-                </button>
-              </div>
-            )}
-
-            {this.state.showEditForm && (
-              <div className="edit-form-buttons">
-                <button className="btn btn-edit" onClick={this.editEntry}>
-                  Edit
-                </button>
-                <button className="btn btn-cancel" onClick={this.cancelEntry}>
-                  Cancel
-                </button>
-              </div>
-            )}
-          </form>
-        )}
-
-        <div className="education-details-container">
-          <article>
-            {this.props.entries.map((entry, index) => {
-              return (
-                <div className="education-entry" key={index}>
-                  <div className="education-entry-header">
-                    <h3>
-                      {entry.credential} - {entry.program}
-                    </h3>
-
-                    <div>
-                      <span>{entry.date} </span>
-                      <button
-                        className="btn btn-delete"
-                        data-id={index}
-                        onClick={this.removeEntry}
-                      >
-                        <i className="fas fa-trash"></i>
-                      </button>
-                      <button
-                        className="btn btn-edit"
-                        data-id={index}
-                        onClick={this.showEditForm}
-                      >
-                        <i className="fas fa-edit"></i>
-                      </button>
-                    </div>
+                  <div>
+                    <span>{entry.date} </span>
+                    <button
+                      className="btn btn-delete"
+                      data-id={index}
+                      onClick={removeEntry}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </button>
+                    <button
+                      className="btn btn-edit"
+                      data-id={index}
+                      onClick={showEditForm}
+                    >
+                      <i className="fas fa-edit"></i>
+                    </button>
                   </div>
-                  <p>
-                    <strong>{entry.school}</strong>
-                  </p>
-
-                  <ul>
-                    {entry.description.map((detail, index) => {
-                      return <li key={index}>{detail}</li>;
-                    })}
-                  </ul>
                 </div>
-              );
-            })}
-          </article>
-        </div>
+                <p>
+                  <strong>{entry.school}</strong>
+                </p>
+
+                <ul>
+                  {entry.description.map((detail, index) => {
+                    return <li key={index}>{detail}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </article>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default EducationSection;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,59 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ResumeHeader from './ResumeHeader';
 import EducationSection from './EducationSection';
 import ExperienceSection from './ExperienceSection';
 import PreviewButton from './PreviewButton';
 
-class Resume extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Hold state of all entries from edit mode
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      city: '',
-      province: '',
-      githubURL: '',
-      linkedinURL: '',
-      portfolioURL: '',
-      education: [],
-      experience: [],
-    };
-  }
+const Resume = () => {
+  // Hold state of all entries from edit mode
+  const [entries, setEntries] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    city: '',
+    province: '',
+    githubURL: '',
+    linkedinURL: '',
+    portfolioURL: '',
+    education: [],
+    experience: [],
+  });
 
   // Capture changes made to input fields
-  onChange = (e) => {
+  const onChange = (e) => {
     let target = e.target.name;
     let value = e.target.value;
 
-    this.setState((state, props) => ({
+    setEntries({
       [target]: value,
-    }));
+    });
   };
 
   // Add a new entry to corresponding resume section by adding to array of related entries
-  addEntry = (entry, section) => {
-    this.setState({
-      [section]: [...this.state[section], entry],
+  const addEntry = (entry, section) => {
+    setEntries({
+      [section]: [...entries[section], entry],
     });
   };
 
   // Delete target entry from state
-  deleteEntry = (index, section) => {
-    let entries = [...this.state[section]];
+  const deleteEntry = (index, section) => {
+    let updatedEntries = [...entries[section]];
 
     if (index !== -1) {
-      entries.splice(index, 1);
-      this.setState({ [section]: entries });
+      updatedEntries.splice(index, 1);
+      setEntries({ [section]: updatedEntries });
     }
   };
 
   // Update an existing entry
-  editEntry = (index, section, updatedEntry) => {
-    let entries = this.state[section];
-    let entry = entries[index];
+  const editEntry = (index, section, updatedEntry) => {
+    let updatedEntries = entries[section];
+    let entry = updatedEntries[index];
     let keys = Object.keys(entry);
 
     // Update all keys within object containing entry details to reflect most recently submitted input
@@ -61,29 +57,27 @@ class Resume extends React.Component {
       entry[key] = updatedEntry[key];
     });
 
-    this.setState({ [section]: entries });
+    setEntries({ [section]: entries });
   };
 
-  render() {
-    return (
-      <div className="resume-container">
-        <PreviewButton />
-        <ResumeHeader onChange={this.onChange} />
-        <EducationSection
-          addEducation={this.addEntry}
-          editEducation={this.editEntry}
-          removeEducation={this.deleteEntry}
-          entries={this.state.education}
-        />
-        <ExperienceSection
-          addExperience={this.addEntry}
-          editExperience={this.editEntry}
-          removeExperience={this.deleteEntry}
-          entries={this.state.experience}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="resume-container">
+      <PreviewButton />
+      <ResumeHeader onChange={onChange} />
+      <EducationSection
+        addEducation={addEntry}
+        editEducation={editEntry}
+        removeEducation={deleteEntry}
+        entries={entries.education}
+      />
+      {/* <ExperienceSection
+        addExperience={addEntry}
+        editExperience={editEntry}
+        removeExperience={deleteEntry}
+        entries={entries.experience}
+      /> */}
+    </div>
+  );
+};
 
 export default Resume;
